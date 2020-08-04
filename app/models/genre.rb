dc49211ab11935a036ab90ddb21088cd7be5f50f
Elijah Kirschen <ekirschen584@gmail.com>
2020-08-04 06:09:14 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,17 +3,17 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    self.songs.length
+    self.songs.count
   end
 
   def artist_count
-    # return the number of artists associated with the genre
-    self.artists.length
+    self.artists.count
   end
 
   def all_artist_names
-    self.artists.map {|a| a.name}
     # return an array of strings containing every musician's name
+    self.artists.map do |t|
+      t.name
+    end
   end
 end
-© 2020 GitHub, Inc.
